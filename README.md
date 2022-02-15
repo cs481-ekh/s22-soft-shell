d5c666a8e31e440e5b80f9ef7f70ef5f85999803
CarsonThompson <51031215+CarsonThompson@users.noreply.github.com>
2022-02-15 15:05:54 -0700

    [#7] Add c function export example and automatic header generation

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # bsu_st_interpreter
 BSU Capstone senior project - Structured Text Interpreter
+
+## Dev Environment Setup
+
+Install cbindgen into cargo for automatic C header file generation:
+
+```shell
+$ cargo install --force cbindgen
+```
+
+It then can be run by calling:
+
+```shell
+$ cbindgen --config cbindgen.toml --lang c --crate st_interpret --output st_interpret.h
+```
+
+Src: https://github.com/eqrion/cbindgen
--- st_interpret/src/lib.rs
+++ st_interpret/src/lib.rs
@@ -1,3 +1,8 @@
+#[no_mangle]
+pub extern "C" fn lib_function_example_add_clib(num_one: usize, num_two: usize) -> usize {
+    num_one + num_two
+}
+
 pub fn lib_function_example_add(num_one: usize, num_two: usize) -> usize {
     num_one + num_two
 }
@@ -5,12 +10,18 @@ pub fn lib_function_example_add(num_one: usize, num_two: usize) -> usize {
 // Unit tests here
 #[cfg(test)]
 mod tests {
-    use crate::lib_function_example_add;
+    use crate::{lib_function_example_add, lib_function_example_add_clib};
 
     #[test]
     fn test_add() {
         let result = lib_function_example_add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_add_c() {
+        let result = lib_function_example_add_clib(2, 22);
+        assert_eq!(result, 24);
     }
 
     #[test]
