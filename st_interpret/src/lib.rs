ece00660320c2a659efe2d6b7d6bb478f8dd62d0
CarsonThompson <51031215+CarsonThompson@users.noreply.github.com>
2022-02-15 14:04:00 -0700

    [#7] Add example lib function and unit tests

--- st_interpret/src/lib.rs
+++ st_interpret/src/lib.rs
@@ -1,8 +1,22 @@
+pub fn lib_function_example_add(num_one: usize, num_two: usize) -> usize {
+    num_one + num_two
+}
+
+// Unit tests here
 #[cfg(test)]
 mod tests {
+    use crate::lib_function_example_add;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
+    fn test_add() {
+        let result = lib_function_example_add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_add_wrong() {
+        let result = 3;
         assert_eq!(result, 4);
     }
 }
