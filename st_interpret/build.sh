d94f920715056989eee44c3e41c15e95085ac627
CarsonThompson <51031215+CarsonThompson@users.noreply.github.com>
2022-02-15 21:08:47 -0700

    [#7] Add build, test, and clean scripts

--- st_interpret/build.sh
+++ st_interpret/build.sh
@@ -1,1 +1,9 @@
-echo "project build"
+# Generates rust lib, static and dynamic C lib
+
+if ! [ -x "$(command -v cargo)" ]; then
+  echo 'Warning: cargo is not installed.' >&2
+  echo 'Installing...' >&2
+  curl https://sh.rustup.rs -sSf | sh
+fi
+
+cargo cbuild || { echo "Build failed" >&2; exit 1; }
--- st_interpret/clean.sh
+++ st_interpret/clean.sh
@@ -1,1 +1,7 @@
-echo "project clean"
+if ! [ -x "$(command -v cargo)" ]; then
+  echo 'Warning: cargo is not installed.' >&2
+  echo 'Installing...' >&2
+  curl https://sh.rustup.rs -sSf | sh
+fi
+
+cargo clean || { echo "cargo clean failed" >&2; exit 1; }
