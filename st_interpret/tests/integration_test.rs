2d46140c22e2fe7a4d4bdb1913b988ebb95b3855
CarsonThompson <51031215+CarsonThompson@users.noreply.github.com>
2022-02-15 21:35:44 -0700

    [#7] Add another test to bump ci to use updated workflow

--- st_interpret/tests/integration_test.rs
+++ st_interpret/tests/integration_test.rs
@@ -6,3 +6,8 @@ use st_interpret;
 fn it_adds_two_integration() {
     assert_eq!(4,  st_interpret::lib_function_example_add(2, 2));
 }
+
+#[test]
+fn it_adds_20_integration() {
+    assert_eq!(22,  st_interpret::lib_function_example_add(20, 2));
+}
