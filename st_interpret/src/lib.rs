ede02f9ea054ee188300e593915ee748130cefe8
CarsonThompson <51031215+CarsonThompson@users.noreply.github.com>
2022-02-16 08:53:23 -0700

    [#14] Add larlpop import and example

--- st_interpret/src/lib.rs
+++ st_interpret/src/lib.rs
@@ -4,10 +4,22 @@ pub fn lib_function_example_add(num_one: usize, num_two: usize) -> usize {
     num_one + num_two
 }
 
+#[macro_use] extern crate lalrpop_util;
+lalrpop_mod!(pub calculator1);
+
 // Unit tests here
 #[cfg(test)]
 mod tests {
-    use crate::{lib_function_example_add};
+    use crate::{lib_function_example_add, calculator1};
+
+    // Testing larlpop functionality
+    #[test]
+    fn calculator1() {
+        assert!(calculator1::TermParser::new().parse("22").is_ok());
+        assert!(calculator1::TermParser::new().parse("(22)").is_ok());
+        assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
+        assert!(calculator1::TermParser::new().parse("((22)").is_err());
+    }
 
     #[test]
     fn test_add() {
