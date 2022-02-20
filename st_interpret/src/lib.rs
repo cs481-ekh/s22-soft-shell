a9dafe37c998c4d29c4c05725155e021016828e1
DouglasTucker <DouglasTucker@u.boisestate.edu>
2022-02-19 20:20:49 -0700

    Revert "Open and Read File"
    This reverts commit e001659f02b22e8c2019259c8f46a3d2f88200a2.

--- st_interpret/src/lib.rs
+++ st_interpret/src/lib.rs
@@ -1,26 +1,7 @@
-use std::fs::File;
-use std::io::BufRead;
-use std::io::BufReader;
-
 mod capi;
 
 pub fn lib_function_example_add(num_one: usize, num_two: usize) -> usize {
     num_one + num_two
-}
-
-pub fn read_file() -> String {
-    let file = File::open("src/simple_test.txt").unwrap();
-    let mut lines = BufReader::new(file).lines();
-    let mut test = String::new();
-
-    loop {
-        if let Some(s) = lines.next() {
-            test.push_str(&s.unwrap());
-        } else {
-            break;
-        }
-    }
-    return test;
 }
 
 #[macro_use]
@@ -32,7 +13,7 @@ mod prog_handle;
 // Unit tests here
 #[cfg(test)]
 mod tests {
-    use crate::{lib_function_example_add, parser, read_file};
+    use crate::{lib_function_example_add, parser};
 
     // Testing larlpop functionality
     #[test]
@@ -54,11 +35,5 @@ mod tests {
     fn test_add_wrong() {
         let result = 3;
         assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn test_open_file() {
-        let a = read_file();
-        assert_eq!(a.is_empty(), false);
     }
 }
