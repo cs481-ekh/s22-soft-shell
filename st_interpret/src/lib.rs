320b52e790e611d7f4e53eb72934c722e7111487
rdmuschamp2 <77304771+rdmuschamp2@users.noreply.github.com>
2022-02-19 20:56:34 -0700

    Closes #21(#27)

--- st_interpret/src/lib.rs
+++ st_interpret/src/lib.rs
@@ -1,11 +1,31 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
 mod capi;
 
 pub fn lib_function_example_add(num_one: usize, num_two: usize) -> usize {
     num_one + num_two
 }
 
+pub fn read_file() -> String {
+    let file = File::open("src/simple_test.txt").unwrap();
+    let mut lines = BufReader::new(file).lines();
+    let mut test = String::new();
+
+    loop {
+        if let Some(s) = lines.next() {
+            test.push_str(&s.unwrap());
+        } else {
+            break;
+        }
+    }
+    return test;
+}
+
 #[macro_use]
 extern crate lalrpop_util;
+
 lalrpop_mod!(pub parser);
 
 mod prog_handle;
@@ -13,7 +33,7 @@ mod prog_handle;
 // Unit tests here
 #[cfg(test)]
 mod tests {
-    use crate::{lib_function_example_add, parser};
+    use crate::{lib_function_example_add, parser, read_file};
 
     // Testing larlpop functionality
     #[test]
@@ -35,5 +55,11 @@ mod tests {
     fn test_add_wrong() {
         let result = 3;
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_open_file() {
+        let a = read_file();
+        assert_eq!(a.is_empty(), false);
     }
 }
