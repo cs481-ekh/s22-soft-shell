e7485aaf7f720b2e88e26b0426dc57a9e9f5bd79
NatalieReece <70127454+NatalieReece@users.noreply.github.com>
2022-02-24 14:35:34 -0700

    Closes #19  (#44)

--- st_interpret/src/prog_handle.rs
+++ st_interpret/src/prog_handle.rs
@@ -1,9 +1,25 @@
 mod prog_handle {
+    use chrono::naive::{NaiveDate, NaiveTime};
     use std::collections::HashMap;
+    use std::time::Duration;
 
     enum VariableValue {
         INT(i16),
         BOOL(bool),
+        BYTE(u8),
+        WORD(u16),
+        UINT(u16),
+        DWORD(u32),
+        DINT(i32),
+        REAL(f32),
+        LREAL(f64),
+        CHAR(u8),
+        WCHAR(u16),
+        STRING(String),
+        TIME(Duration),
+        LTIME(Duration),
+        DATE(NaiveDate),
+        TIME_OF_DAY(NaiveTime),
     }
 
     enum VariableKind {
