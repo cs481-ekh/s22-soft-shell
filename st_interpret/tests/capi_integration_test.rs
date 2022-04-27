ad492282272d54249b48c5221758009b82ea663c
rdmuschamp2 <77304771+rdmuschamp2@users.noreply.github.com>
2022-04-27 09:50:44 -0600

    Closes #110 (#181)

--- st_interpret/tests/capi_integration_test.rs
+++ st_interpret/tests/capi_integration_test.rs
@@ -182,4 +182,273 @@ mod capi_test {
         );
         result.success();
     }
+
+    #[test]
+    /// Test the result of addition
+    fn test_add_var_int() {
+        let mut result = assert_c! {
+            #include <st_interpret.h>
+            #include <assert.h>
+            #include <string.h>
+            #include <stdio.h>
+
+            int main() {
+                printf("starting test\n");
+                ProgHandlePointer handle = st_program_load("tests/test_inputs/st_subset_3-4/03_add.st");
+                printf("load was called\n");
+                st_program_run(&handle);
+                printf("run was called\n");
+                VariableNameInfo * result = get_var(&handle, "a");
+                printf("get variable was called\n");
+                assert(!strcmp(result->name, "a"));
+                assert(!strcmp(result->value, "int: 3"));
+                return 0;
+            }
+        };
+        println!(
+            "{:?}",
+            std::str::from_utf8(&(result.assert().get_output().stdout)).unwrap()
+        );
+        result.success();
+    }
+
+    #[test]
+    /// Batch test subset 1
+    fn capi_batch_subset_1() {
+        let mut result = assert_c! {
+            #include <st_interpret.h>
+            #include <assert.h>
+            #include <string.h>
+            #include <stdio.h>
+            #include <dirent.h>
+
+            int capi_batch_test(char *string);
+
+            int main() {
+                capi_batch_test("tests/test_inputs/execution/st_subset_1");
+                return 0;
+            }
+
+            int capi_batch_test(char *string) {
+                DIR *folder;
+                struct dirent *entry;
+
+                // open the file path
+                folder = opendir(string);
+
+                if (folder == NULL) {
+                    perror("Unable to read directory");
+                    return 1;
+                }
+
+                // loop through all of the files located in file path
+                while ((entry = readdir(folder)) != NULL) {
+                    if (entry->d_type == DT_REG) {
+                        printf("starting test %s\n", entry->d_name);
+
+                        char destination[] = "tests/test_inputs/execution/st_subset_1/";
+
+                        char path[255]= { '\0' };
+                        strcat(path,destination);
+                        strcat(path, entry->d_name);
+
+
+                        ProgHandlePointer handle = st_program_load(path);
+                        printf("load was called\n");
+                        st_program_run(&handle);
+                        printf("run was called\n");
+                        VariableNameInfo * bool_result = get_var(&handle, "ST_TESTING_RESULT");
+                        assert(!strcmp(bool_result->value, "bool: true"));
+                    }
+                }
+                closedir(folder);
+                return 0;
+            }
+        };
+        println!(
+            "{:?}",
+            std::str::from_utf8(&(result.assert().get_output().stdout)).unwrap()
+        );
+        result.success();
+    }
+
+    #[test]
+    /// Batch test subset 3 and 4
+    fn capi_batch_subset_3_4() {
+        let mut result = assert_c! {
+            #include <st_interpret.h>
+            #include <assert.h>
+            #include <string.h>
+            #include <stdio.h>
+            #include <dirent.h>
+
+            int capi_batch_test(char *string);
+
+            int main() {
+                capi_batch_test("tests/test_inputs/execution/st_subset_3-4");
+                return 0;
+            }
+
+            int capi_batch_test(char *string) {
+                DIR *folder;
+                struct dirent *entry;
+
+                // open the file path
+                folder = opendir(string);
+
+                if (folder == NULL) {
+                    perror("Unable to read directory");
+                    return 1;
+                }
+
+                // loop through all of the files located in file path
+                while ((entry = readdir(folder)) != NULL) {
+                    if (entry->d_type == DT_REG) {
+                        printf("starting test %s\n", entry->d_name);
+
+                        char destination[] = "tests/test_inputs/execution/st_subset_3-4/";
+
+                        char path[255]= { '\0' };
+                        strcat(path,destination);
+                        strcat(path, entry->d_name);
+
+
+                        ProgHandlePointer handle = st_program_load(path);
+                        printf("load was called\n");
+                        st_program_run(&handle);
+                        printf("run was called\n");
+                        VariableNameInfo * bool_result = get_var(&handle, "ST_TESTING_RESULT");
+                        assert(!strcmp(bool_result->value, "bool: true"));
+                    }
+                }
+                closedir(folder);
+                return 0;
+            }
+        };
+        println!(
+            "{:?}",
+            std::str::from_utf8(&(result.assert().get_output().stdout)).unwrap()
+        );
+        result.success();
+    }
+
+    #[test]
+    /// Batch test subset 5 and 6
+    fn capi_batch_subset_5_6() {
+        let mut result = assert_c! {
+            #include <st_interpret.h>
+            #include <assert.h>
+            #include <string.h>
+            #include <stdio.h>
+            #include <dirent.h>
+
+            int capi_batch_test(char *string);
+
+            int main() {
+                capi_batch_test("tests/test_inputs/execution/st_subset_5-6");
+                return 0;
+            }
+
+            int capi_batch_test(char *string) {
+                DIR *folder;
+                struct dirent *entry;
+
+                // open the file path
+                folder = opendir(string);
+
+                if (folder == NULL) {
+                    perror("Unable to read directory");
+                    return 1;
+                }
+
+                // loop through all of the files located in file path
+                while ((entry = readdir(folder)) != NULL) {
+                    if (entry->d_type == DT_REG) {
+                        printf("starting test %s\n", entry->d_name);
+
+                        char destination[] = "tests/test_inputs/execution/st_subset_5-6/";
+
+                        char path[255]= { '\0' };
+                        strcat(path,destination);
+                        strcat(path, entry->d_name);
+
+
+                        ProgHandlePointer handle = st_program_load(path);
+                        printf("load was called\n");
+                        st_program_run(&handle);
+                        printf("run was called\n");
+                        VariableNameInfo * bool_result = get_var(&handle, "ST_TESTING_RESULT");
+                        assert(!strcmp(bool_result->value, "bool: true"));
+                    }
+                }
+                closedir(folder);
+                return 0;
+            }
+        };
+        println!(
+            "{:?}",
+            std::str::from_utf8(&(result.assert().get_output().stdout)).unwrap()
+        );
+        result.success();
+    }
+
+    #[test]
+    /// Batch test subset 7
+    fn capi_batch_subset_7() {
+        let mut result = assert_c! {
+            #include <st_interpret.h>
+            #include <assert.h>
+            #include <string.h>
+            #include <stdio.h>
+            #include <dirent.h>
+
+            int capi_batch_test(char *string);
+
+            int main() {
+                capi_batch_test("tests/test_inputs/execution/st_subset_7");
+                return 0;
+            }
+
+            int capi_batch_test(char *string) {
+                DIR *folder;
+                struct dirent *entry;
+
+                // open the file path
+                folder = opendir(string);
+
+                if (folder == NULL) {
+                    perror("Unable to read directory");
+                    return 1;
+                }
+
+                // loop through all of the files located in file path
+                while ((entry = readdir(folder)) != NULL) {
+                    if (entry->d_type == DT_REG) {
+                        printf("starting test %s\n", entry->d_name);
+
+                        char destination[] = "tests/test_inputs/execution/st_subset_7/";
+
+                        char path[255]= { '\0' };
+                        strcat(path,destination);
+                        strcat(path, entry->d_name);
+
+
+                        ProgHandlePointer handle = st_program_load(path);
+                        printf("load was called\n");
+                        st_program_run(&handle);
+                        printf("run was called\n");
+                        VariableNameInfo * bool_result = get_var(&handle, "ST_TESTING_RESULT");
+                        assert(!strcmp(bool_result->value, "bool: true"));
+                    }
+                }
+                closedir(folder);
+                return 0;
+            }
+        };
+        println!(
+            "{:?}",
+            std::str::from_utf8(&(result.assert().get_output().stdout)).unwrap()
+        );
+        result.success();
+    }
 }
